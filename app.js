c08577129fd66fe81b481ed856401ee9442669c9
Amit Ayalon <ayalon1amit@gmail.com>
2021-01-04 22:32:49 +0200

    -change the declaring place for the elements -setting up the moveMade algorithm

--- app.js
+++ app.js
@@ -1,15 +1,41 @@
+let rockElem;
+let paperElem;
+let scissorsElem;
+
 window.onload = function () {
-    const rock = document.getElementById("rock");
-    const paper = document.getElementById("paper");
-    const scissors = document.getElementById("scissors");
+    rockElem = document.getElementById("rock");
+    paperElem = document.getElementById("paper");
+    scissorsElem = document.getElementById("scissors");
 
-    rock.onclick = function () { moveMade(rock.id) };
-    paper.onclick = function () { moveMade(paper.id) };
-    scissors.onclick = function () { moveMade(scissors.id) };
-
+    rockElem.onclick = function () { moveMade(rock.id) };
+    paperElem.onclick = function () { moveMade(paper.id) };
+    scissorsElem.onclick = function () { moveMade(scissors.id) };
 }
 
 function moveMade(move) {
-    console.log(move);
+    console.log("The player played " + move);
+    let movesDict = {
+        "rock": 0,
+        "paper": 1,
+        "scissors": 2
+    };
+    let playerMoveId = movesDict[move];
+    let compMoveId = Math.floor(Math.random(3));
+
+    if (playerMoveId == compMoveId) {
+        //tie
+    } else if (playerMoveId == 0 && compMoveId == 2) {
+        //player win - rock smash scissors
+    } else if (playerMoveId == 2 && compMoveId == 0) {
+        //computer win - rock smash scissors
+    } else if (playerMoveId == 1 && compMoveId == 0) {
+        //player win - paper cover rock
+    } else if (playerMoveId == 0 && compMoveId == 1) {
+        //computer win - paper cover rock
+    } else if (playerMoveId == 2 && compMoveId == 1) {
+        //player win - scissors cut paper
+    } else if (playerMoveId == 1 && compMoveId == 2) {
+        //computer win - scissors cut paper
+    } 
 }
 
